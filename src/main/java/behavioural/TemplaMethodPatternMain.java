032fd91f2643fe2f4a20d97052bc281993792a3f
apoorprogrammer <apoorprogrammer@gmail.com>
2019-01-22 00:23:45 +0100

    Create TemplateMethod explanation

---
+++ src/main/java/behavioural/TemplaMethodPatternMain.java
@@ -0,0 +1,24 @@
+package behavioural;
+
+public class TemplaMethodPatternMain {
+
+	/**
+	 * 
+	 * @author David de Miguel Otero
+	 * 
+	 * Template method pattern allow us to define a script of steps and defer this step's definition
+	 * in subclasses
+	 * 
+	 * We are going to use an example about how a template method pattern is used when we have two classes 
+	 * represents two concepts nearly.
+	 * We create an abstract class named SportEvent which implements the Template Method and will have a 
+	 * steps series.
+	 * After that, we created two classes BasketballSportEvent and FootballSportEvent each them defines 
+	 * specifically each step.
+	 */
+	
+	public static void main(String[] args) {
+
+	}
+
+}
