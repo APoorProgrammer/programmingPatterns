2ad329114523e5c9d9be2c4b0ca4fe8addaa62ec
apoorprogrammer <apoorprogrammer@gmail.com>
2019-01-21 17:23:32 +0100

    Add Constants class

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,16 @@
+package constants;
+
+public class Constants {
+
+	//Constants to Adapter pattern's example
+	public static final String FIGO_NAME = "Luis Figo";
+	public static final Double FIGO_SALARY = 600000.99;
+	public static final Integer BARCELONA_FIGO_DORSAL = 7;
+	public static final String VISCA_BARCA = "¡¡¡Visca el Barça y Visca Catalunya!!!";
+	public static final Integer RM_FIGO_DORSAL = 10;
+	public static final String ZIDANE_NAME = "Zinedine Zidan";
+	public static final Double ZIDANE_SALARY = 1200000.99;
+	public static final Integer ZIDANE_NUMBER = 5;
+	public static final String HALA_MADRID = "¡¡¡Hala Madrid y nada más!!!";
+	
+}
