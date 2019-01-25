e3baf9e99ed8bf45d9434b141f9650b02b1ea103
apoorprogrammer@gmail.com <apoorprogrammer@gmail.com>
2019-01-25 11:26:50 +0100

    creating Facade Pattern example

---
+++ src/main/java/structural/FacadePatternMain.java
@@ -0,0 +1,23 @@
+package structural;
+
+public class FacadePatternMain {
+
+	/**
+	 * 
+	 * @author David de Miguel Otero
+	 * 
+	 * This pattern is used when we want to centralize a set of similar interfaces creating an unique interface
+	 * which unified the susbsystems.
+	 * 
+	 * In our case we are simulating a negotiation between TV platform(the client) which wants to buy 
+	 * LaLiga's broadcast's rights
+	 * In this example there will be many teams with similar interface and instead of use each them we are going to
+	 * create a LaLiga interface which works as a facade between the TV buyer and the teams
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+
+	}
+
+}
