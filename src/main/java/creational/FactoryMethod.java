972424a5d3f1e7bde38dd764c7476b98461629b8
apoorprogrammer@gmail.com <apoorprogrammer@gmail.com>
2019-01-27 12:10:06 +0100

    Creation FactoryMethod pattern

---
+++ src/main/java/creational/FactoryMethod.java
@@ -0,0 +1,23 @@
+package creational;
+
+public class FactoryMethod {
+
+	/**
+	 * 
+	 * @author David de Miguel Otero
+	 * 
+	 * Factory method pattern is used when we want to abstract creations' object from client and defer 
+	 * this creation in subclassses which implements it.
+	 * 
+	 * In our example we are going to create a youth academy's interface with EnumType which we can choose 
+	 * what type of player we need for our foremost team(client).
+	 * For our foremost team the formation(creation) is not interesting, it only need a player(object) 
+	 * of this type(EnumType)
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
